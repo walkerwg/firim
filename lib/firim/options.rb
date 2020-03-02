3b5ce4afe1ebacc0c3b6026168132a5e3ffdb2c0
Walkerwg <907029564@qq.com>
2020-03-02 17:06:45 +0800

    Update options.rb
    修改fir域名

--- lib/firim/options.rb
+++ lib/firim/options.rb
@@ -11,7 +11,7 @@ module Firim
                                      description: "The fir platform, support ios/android"),
         # fir.im api url
         FastlaneCore::ConfigItem.new(key: :firim_api_url,
-                                     default_value: "https://api.fir.im",
+                                     default_value: "https://www.betaqr.com",
                                      description: "fir.im api URL"),
         # firim info
         FastlaneCore::ConfigItem.new(key: :firim_api_token,
